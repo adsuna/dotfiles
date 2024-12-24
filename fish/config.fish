b25710996edcf35696d9f96fd257daa0207b8ae8
adsuna <adityanair2120@gmail.com>
2024-12-24 20:27:40 +0530

    last update :/

--- fish/config.fish
+++ fish/config.fish
@@ -7,13 +7,17 @@ function fish_greeting
 alias yank="yay -S"
 alias scan="yay -q"
 alias yeet="yay -Rns"
-alias z="zeditor"
 alias hexec="hyprctl dispatch exec"
 alias dots=~/scripts/dots.sh
-
 
 end
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# pyenv
+pyenv init - fish | source
+
+# zoxide
+zoxide init --cmd cd fish | source
