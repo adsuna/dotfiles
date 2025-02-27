244d06758eea8ffc52e013e84b080eb9f8f7d389
adsuna <adityanair2120@gmail.com>
2025-02-27 22:44:30 +0530

    updated configs

--- .config/hypr/scripts/selectwallpaper.sh
+++ .config/hypr/scripts/selectwallpaper.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 wallpaper=$(kdialog --getopenfilename ~/Pictures/Wallpapers "*.jpg *.png *.gif" --title "Pick a Wallpaper")
-
-if [ -f "$wallpaper" ]; then
-    swww img "$wallpaper" --transition-step 100 --transition-fps 165 --transition-type grow --transition-angle 30 --transition-duration 1 && cp "$wallpaper" ~/.config/background
+cp "$wallpaper" ~/Pictures/background
+setwal=~/Pictures/background
+if [ -f "$setwal" ]; then
+    swww img "$setwal" --transition-step 100 --transition-fps 165 --transition-type grow --transition-angle 30 --transition-duration 1
 fi
