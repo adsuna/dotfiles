1ff0780d93f4d75d15e7891c0367492926f46d19
adsuna <adityanair2120@gmail.com>
2025-03-03 21:47:53 +0530

    updated configs

--- .config/waybar/scripts/temps.sh
+++ .config/waybar/scripts/temps.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-# Nerd font icons
+# nerd font icons
 CPU_ICON=""
 GPU_ICON="󰍹"
 
-# Get CPU temp from k10temp-pci-00c3
+# get cpu temp from k10temp-pci-00c3
 CPU_TEMP=$(sensors | awk '/Tctl:/ {gsub("+", ""); print $2}')
-CPU_TEMP=${CPU_TEMP%°C}  # Remove the °C suffix if present
+CPU_TEMP=${CPU_TEMP%°C}  # remove the °C suffix if present
 
-# Get GPU temp using nvidia-smi
+# get gpu temp using nvidia-smi
 GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader | awk '{print $1}')
-GPU_TEMP=${GPU_TEMP%°C}  # Remove the °C suffix if present
+GPU_TEMP=${GPU_TEMP%°C}  # remove the °C suffix if present
 
-# Function to get color based on temperature
-get_color() {
+# function to get color based on temperature
+get_colored_icon_temp() {
   local temp=$1
+  local icon=$2
+
   if (( $(echo "$temp < 60" | bc -l) )); then
-    echo "<span color='#00FF00'>$temp°C</span>"  # Green for low temp
+    echo "<span color='#A6E3A1'>$icon $temp°C</span>"  # green for low temp
   elif (( $(echo "$temp < 80" | bc -l) )); then
-    echo "<span color='#FFFF00'>$temp°C</span>"  # Yellow for moderate temp
+    echo "<span color='#F9E2AF'>$icon $temp°C</span>"  # yellow for moderate temp
   else
-    echo "<span color='#FF0000'>$temp°C</span>"  # Red for high temp
+    echo "<span color='#F38BA8'>$icon $temp°C</span>"  # red for high temp
   fi
 }
 
-# Get colored temperature strings
-CPU_TEMP_COLORED=$(get_color $CPU_TEMP)
-GPU_TEMP_COLORED=$(get_color $GPU_TEMP)
+# get colored temperature strings with icons
+CPU_TEMP_COLORED=$(get_colored_icon_temp $CPU_TEMP "$CPU_ICON")
+GPU_TEMP_COLORED=$(get_colored_icon_temp $GPU_TEMP "$GPU_ICON")
 
-# Output JSON format
-echo -n "{\"text\": \"$CPU_ICON $CPU_TEMP_COLORED  $GPU_ICON $GPU_TEMP_COLORED\"}"
+# output json format
+echo -n "{\"text\": \"$CPU_TEMP_COLORED  $GPU_TEMP_COLORED\"}"
