7d9c0f87dccdd1b80550ccaae8bdd07288d8707d
adsuna <adityanair2120@gmail.com>
2025-10-30 23:08:46 +0530

    update dotfiles 2025-10-30 23:08:46

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -3,35 +3,7 @@
 DIR="$HOME/Pictures/Screenshots"
 mkdir -p "$DIR"
 
-TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-FILE="$DIR/screenshot-$TIMESTAMP.png"
+FILENAME="screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
 
-case "$1" in
-  area)
-    grimblast --freeze copysave area "$FILE"
-    satty --filename "$FILE" \
-          --output-filename "$FILE" \
-          --initial-tool brush \
-          --copy-command wl-copy \
-          --save-after-copy \
-          --early-exit \
-          --disable-notifications
-    echo "saved area screenshot: $FILE"
-    ;;
-
-  screen)
-    grimblast --freeze copysave screen "$FILE"
-    satty --filename "$FILE" \
-          --output-filename "$FILE" \
-          --initial-tool brush \
-          --copy-command wl-copy \
-          --save-after-copy \
-          --disable-notifications \
-          --early-exit
-    echo "saved full screen screenshot: $FILE"
-    ;;
-
-  *)
-    echo "usage: $0 {area|screen}"
-    ;;
-esac
+# take a region screenshot, include cursor, save, and send notif
+gscreenshot -cs -f "$DIR/$FILENAME" -n
