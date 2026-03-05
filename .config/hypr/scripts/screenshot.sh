70cc71506ea1e95b25626cf11908b7e09420d27a
adsuna <adityanair2120@gmail.com>
2026-03-05 23:14:24 +0530

    fresh start

--- .config/hypr/scripts/screenshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$HOME/Pictures/Screenshots"
-mkdir -p "$DIR"
-
-FILENAME="screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
-
-# take a region screenshot, include cursor, save, and send notif
-gscreenshot -cs -f "$DIR/$FILENAME" -n
