c905b8f621d1e6acb60af911773ab4f2dab6c2c9
Aditya Nair <adnair@redhat.com>
2026-03-09 10:44:42 +0530

    update dotfiles 2026-03-09 10:44:42

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@ set -e
 
 # folders to sync
 folders=(
-  fish
   foot
   hypr
   vicinae
@@ -14,16 +13,16 @@ folders=(
 repo_dir="$HOME/dotfiles/.config"
 config_dir="$HOME/.config"
 
+cd "$HOME/dotfiles"
+git switch fedora
 echo "[*] syncing configs..."
 for f in "${folders[@]}"; do
   rsync -av --delete "$config_dir/$f/" "$repo_dir/$f/"
 done
-
-cd "$HOME/dotfiles"
 
 echo "[*] committing changes..."
 git add .
 git commit -m "update dotfiles $(date '+%Y-%m-%d %H:%M:%S')" || echo "no changes to commit"
 
 echo "[*] pushing..."
-git push
+git push -u origin fedora
